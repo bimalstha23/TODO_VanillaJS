c548b783c98ccc83f04d62212baa120ae25cfdba
Bimal Shrestha <bimalshrestha074@gmail.com>
2022-10-28 10:09:14 +0545

    update task

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-// import 'Style/Style.css';
-
-// const TODOlist = []
 
 
-const TODOlist =JSON.parse(window.localStorage.getItem('tasks')) || [];
+const TODOlist = JSON.parse(window.localStorage.getItem('tasks')) || [];
 
 
 window.addEventListener('load', () => {
   const inputField = document.querySelector('#taskInput_field');
-  console.log(inputField);
   const form = document.querySelector('#taskInput_form');
 
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const task = inputField.value;
-    if(task==='' || task===null) return;
-    
+    if (task === '' || task === null) return;
+
     const newTask = {
       id: Date.now(),
       text: task,
@@ -35,7 +31,7 @@ window.addEventListener('load', () => {
 
 function renderTasks() {
   const tasks = document.querySelector('.tasks');
-  tasks.innerHTML='';
+  tasks.innerHTML = '';
 
 
   TODOlist.forEach((task) => {
@@ -45,11 +41,23 @@ function renderTasks() {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = task.done;
+
+    //update task status
+    checkbox.addEventListener('change', () => {
+      task.done = !task.done;
+      window.localStorage.setItem('tasks', JSON.stringify(TODOlist));
+      renderTasks();
+    })
+
+
     const taskbox = document.createElement('div');
     taskbox.classList.add('taskbox');
     const text = document.createElement('input');
     text.type = 'text';
     text.value = task.text;
+    if (task.done) {
+      text.classList.add('done');
+    }
     text.setAttribute('readonly', true);
     const actions = document.createElement('div');
     actions.classList.add('actions');
@@ -65,8 +73,21 @@ function renderTasks() {
     const editicon = document.createElement('span');
     editicon.classList.add('icon-pencil');
     edit.appendChild(editicon);
-    task_el.append(checkbox,taskbox);
-    taskbox.append(text,actions);
+    task_el.append(checkbox, taskbox);
+    taskbox.append(text, actions);
     actions.append(edit, deleteButton);
+
+    //update task text
+    edit.addEventListener('click', () => {
+      text.removeAttribute('readonly');
+      text.focus();
+      text.addEventListener('blur', () => {
+        text.setAttribute('readonly', true);
+        task.text = text.value;
+        window.localStorage.setItem('tasks', JSON.stringify(TODOlist));
+        renderTasks();
+      })
+    })
+    
   })
 }
